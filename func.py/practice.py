96f77e2fff985b03ee57e77f25066dc129564ae4
utsav gangadiya <utsavgangadiya886@gmail.com>
2026-07-18 20:04:00 +0530

    practice func

---
+++ func.py/practice.py
@@ -0,0 +1,26 @@
+num_list =[10,11,12,13,14,15,16,17,18,19]
+
+def len_list (list):
+    print( len(list))
+
+len_list(num_list)
+
+# 10
+
+def list_item (list):
+    for i in list:
+        print(i,end=" ")
+
+list_item(num_list)
+print()
+# 10 11 12 13 14 15 16 17 18 19 
+
+# factorial
+
+def cal_fact(num):
+    fact = 1 
+    for i in range (1,num+1):
+        fact *= i
+    return fact
+
+print(cal_fact(5))
