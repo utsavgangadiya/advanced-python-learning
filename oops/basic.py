ff1d732d0ea49509e42247a3397b4f16a75a816c
utsav gangadiya <utsavgangadiya886@gmail.com>
2026-07-31 16:09:05 +0530

    practice oops

---
+++ oops/basic.py
@@ -0,0 +1,20 @@
+# class and object 
+
+class products :
+    unit = 100
+    price = 299
+    types = 10
+
+info = products()
+print(products.unit)
+print(products.types)
+
+# constructor
+
+class student :
+    def __init__(self,fullname):
+        self.name = fullname
+        print("add new student data...")
+
+s1 = student("utsav gangadiya")
+print(s1.name)
